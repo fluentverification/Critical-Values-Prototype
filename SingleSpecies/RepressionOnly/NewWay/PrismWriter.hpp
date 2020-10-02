2569486e4cbb99fd3380d13397d074a7aedef4c8
Porter <porter.giles99@gmail.com>
2020-10-02 19:37:03 -0400

    added repression only
    Signed-off-by: Porter <porter.giles99@gmail.com>

---
+++ SingleSpecies/RepressionOnly/NewWay/PrismWriter.hpp
@@ -0,0 +1,8 @@
+#ifndef PRISM_WRITER_HPP
+#define PRISM_WRITER_HPP
+
+void writeFile(std::string FileName, int InitialValue, int epsilon);
+
+void writeFile(std::string filename, std::vector<int> pt);
+
+#endif //PRISM_WRITER_HPP
---
+++ SingleSpecies/RepressionOnly/PrismReader.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+###################################
+#Set Variables
+###################################
+
+#This is only for Porter
+porter=true
+
+#Max value for Prism models
+max=100
+
+#These are the epsilon values for prism
+values=(100 50 25 20 10 5 4 2 1)
+
+#Name of resulting file
+file="result"
+
+###################################
+#PreRun checks
+###################################
+
+#Check for the prism command
+if ! command prism -v &> /dev/null; then
+	if $porter; then
+		echo "sourcing .profile"
+		source ~/.profile
+	else
+		echo "prism command not found"
+		echo "aborting"
+		exit
+	fi
+fi
+
+###################################
+#Run Prism 
+###################################
+
+#Run prism for each value of set values
+for i in "${values[@]}"; do 
+
+	#Run Prism for next value of set values 
+	./PrismWriter  temp.prism "$max" "$i"
+
+	Result="$(prism temp.prism ../SSDegradation.csl | grep "Result: " | sed s/^Result:.// | sed s/"............(value in the initial state)"//)"
+
+	echo "$Result $(expr $max / $i)" >> "$file".txt
+
+done
+
+rm temp.prism
