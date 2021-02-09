6937f8ff83da7d936cedaf73531b9245a03dfb34
Porter Giles <porter.giles99@gmail.com>
2021-02-08 22:33:19 -0700

    Added results from threshold spacing expieriments
    Signed-off-by: Porter Giles <porter.giles99@gmail.com>

--- CriticalPointFinder/non-cumulative/main.cpp
+++ CriticalPointFinder/non-cumulative/main.cpp
@@ -6,20 +6,16 @@
 int main()
 {
 
-    //int IPTG, LacI;
+    int IPTG, LacI;
     double threshold;
     std::string name;
 
-    //std::cout << "Enter in the amount of IPTG/aTc: ";
-    //std::cin >> IPTG;
-    //std::cout << "Enter the final amount of LacI/TetR to be recorded to: ";
-    //std::cin >> LacI;
+    std::cout << "Enter in the amount of IPTG/aTc: ";
+    std::cin >> IPTG;
+    std::cout << "Enter the final amount of LacI/TetR to be recorded to: ";
+    std::cin >> LacI;
     std::cout << "Enter in the name of the resulting file (include .txt): ";
     std::cin >> name;
-
-   int IPTG = 200;
-   int LacI = 200;
-   
 
     /**When creating a LacIgraph object, enter in the following format************************************************
     ***** <name of object>(fixed value of IPTG, amount we want to record LacI to, name of graph as .txt file)*********/
