5c4eeffaac1940c7ca34f0c6d6b80d9b885d97d3
Porter Giles <porter.giles99@gmail.com>
2021-04-20 12:18:14 -0600

    individual state analysis
    Signed-off-by: Porter Giles <porter.giles99@gmail.com>

--- tickets/state_analysis/DegradationRange.py
+++ tickets/state_analysis/DegradationRange.py
@@ -4,6 +4,11 @@
 def deg( level ):
     return .0075 * 50 / (50 - level)
 
+def degrange(low, high):
+    return .0075 * high / (high - low)
 
-for i in range(15, 31):
-    print(deg(i))
+
+#for i in range(31, 50):
+#    print(degrange(i, 50))
+
+print(degrange(30, 50))
--- tickets/state_analysis/RepressionFormula.py
+++ tickets/state_analysis/RepressionFormula.py
@@ -4,6 +4,11 @@
 def p_t( level ):
     return 0.99/(0.25*(level)*(level) + 1.99)
 
+def repRange(low, high):
+    return p_t(low) * ( 10 / (high - low) )
 
-for i in range(15, 31):
-    print(p_t(i) * ( 10 / (50 - i) ))
+
+#for i in range(51, 100):
+#    print(repRange(i, 100))
+
+print(repRange(50, 100))
