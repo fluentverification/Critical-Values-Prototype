46be3551087f2b2b456ac714640a4dbc27cd29e0
Porter Giles <porter.giles99@gmail.com>
2021-08-11 23:41:02 -0600

    worked on mapping out the speed ind model
    Signed-off-by: Porter Giles <porter.giles99@gmail.com>

--- C-Element/speed-ind/scripts/critical-value.sh
+++ C-Element/speed-ind/scripts/critical-value.sh
@@ -67,9 +67,93 @@ fi
 
 tolerance=$1
 
-######        ######
-###  Species S1  ###
-######        ######
+######              ######
+###  Species S1 in S2  ###
+######              ######
+eq1='(10*(0.099/(1.99+((0.5*'
+eq2=')^2))*10+0.099/(1.99+((0.5*0)^2))*10))'
+max=250
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 in S2 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species S2 in S3  ###
+######              ######
+eq1='(10*(0.099/(1.99+((0.5*S4)^2))*10+0.099/(1.99+((0.5*S2)^2))*10))'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=100
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species S2 in S4  ###
+######              ######
+eq1='(10*(0.099/(1.99+(0.5*'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=100
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species S3 in S2  ###
+######              ######
+eq1='(10*(0.099/(1.99+((0.5*0)^2))*10+0.099/(1.99+((0.5*' 
+eq2=')^2))*10))'
+max=150
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species S3 in Z   ###
+######              ######
+eq1='(10*(0.099/(1.99+(0.5*'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=150
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species S4 in S3  ###
+######              ######
+eq1='(10*(0.099/(1.99+((0.5*S4)^2))*10+0.099/(1.99+((0.5*S2)^2))*10))'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=120
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species S4 in Z   ###
+######              ######
 eq1='(10*(0.099/(1.99+(0.5*'
 eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
 max=120
@@ -81,10 +165,44 @@ echo "#### Species S1 ####"
 print_thresholds $eq1 $eq2 $max $indicator
 echo "$max is the last Thresholds"
 
-######        ######
-###  Species S2  ###
-######        ######
+######              ######
+###  Species Y in S1   ###
+######              ######
+eq1='(10*(0.099/(1.99+(0.5*'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=90
 
-######        ######
-###  Species S1  ###
-######        ######
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species Z in CC   ###
+######              ######
+eq1='(10*(0.099/(1.99+(0.5*'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=90
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
+
+######              ######
+###  Species XX in S1  ###
+######              ######
+eq1='(10*(0.099/(1.99+(0.5*'
+eq2=')^2)*10+0.099/(1.99+(0.5*0)^2)*10))'
+max=90
+
+indicator=$(findmax $eq1 $eq2 $max)
+indicator=$( echo "$indicator * $tolerance" | bc -l)
+
+echo "#### Species S1 ####"
+print_thresholds $eq1 $eq2 $max $indicator
+echo "$max is the last Thresholds"
