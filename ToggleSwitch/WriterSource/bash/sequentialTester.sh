8a731ffffb361d15a00bc5e4419771da85dedede
Porter Giles <porter.giles99@gmail.com>
2022-01-21 15:47:43 -0700

    updated sequentialTester.sh so that it doesn't test models that have already been tested
    Signed-off-by: Porter Giles <porter.giles99@gmail.com>

--- ToggleSwitch/WriterSource/bash/sequentialTester.sh
+++ ToggleSwitch/WriterSource/bash/sequentialTester.sh
@@ -4,24 +4,32 @@
 # that if the program terminates early for some reason
 # we can pick up where we left off.
 
-TETRFIXED=",0,50,1,2"
-LACIFIXED=",0,60,80,20,36"
+# For reference: the control model with property toggle_IPTG_0
+# is 3.383226110378722E-13
+
+# 3 states on each species
+TETRFIXED=",0,50,1,2,3,4,5"
+LACIFIXED=",0,60,80,20,36,77,45,26"
 tempfile="/tmp/temp.prism"
 csl=$1
+
+###--- normalize array ---###
+read -a TetR_trs <<< $(echo $TETRFIXED | sed 's/^,//' | sed 's/,/ /g')
+read -a LacI_trs <<< $(echo $LACIFIXED | sed 's/^,//' | sed 's/,/ /g')
 
 ###--- Start Of Script---###
 
 for TETR in {0..50}; do
   # Make sure we aren't checking models that we already have
   # checked
-  if echo $TETRFIXED | grep -q $TETR ; then 
+  if echo $TETRFIXED | grep -qw $TETR ; then 
     continue
   fi
 
   for LACI in {0..80}; do
     # Make sure we aren't checking models that we already have
     # checked
-    if echo $LACIFIXED | grep -q $LACI ; then 
+    if echo $LACIFIXED | grep -qw $LACI ; then 
       continue
     fi
     # Make model
