c797495125cdfdc8eaf8691922296b6b90df8c3b
Porter Giles <porter.giles99@gmail.com>
2022-04-03 23:45:24 -0600

    Made progress on jani parser
    Signed-off-by: Porter Giles <porter.giles99@gmail.com>

--- janiParser/parseJani.py
+++ janiParser/parseJani.py
@@ -3,6 +3,34 @@
 import json
 import sys
 
+###### Class Module #######
+class Module:
+  def __init__(self, name, pr, dr):
+    self.name = name
+    self.productionRate = pr
+    self.degradationRate = dr
+
+  def makeModule(self):
+    print("module " + self.name) 
+    print("endmodule //" + self.name) 
+
+
+###### Class AbstractModel ######
+class AbstractModel:
+  def __init__(self, infile, outfile):
+    self.infile = infile
+    self.outfile = outfile
+    self.modules = []
+
+  def makeModel(self):
+    for x in self.modules:
+      x.makeModule() 
+      print()
+
+#################################
+#########    Main    ############
+#################################
+
 n = len(sys.argv)
 
 if n != 2:
@@ -11,7 +39,78 @@ if n != 2:
 
 filename = sys.argv[1]
 
+myAbstract = AbstractModel(filename, "somefile.prism")
+
 with open(filename, 'r') as janiFile:
   print("Parsing file: ", filename)
   janiData = json.load(janiFile)
-  print(janiData)
+
+  # 1st grab the various automaton (modules)
+  automatons = []
+
+  for x in janiData["system"]["elements"]:
+    automatons.append(x["automaton"])
+
+  # 2nd determine format based on presence of reaction_rates module
+  
+  #DEF MODEL STYLES
+
+  STYLE_TRAD = 1
+  STYLE_SBML = 2
+  STYLE = STYLE_TRAD
+
+  for x in automatons:
+    if x == "reaction_rates":
+      STYLE = STYLE_SBML
+
+  # create empty Modules list
+  Modules = ()
+      
+  # 3rd if reaction rates are in their own module then parse synchronizations
+  if STYLE == STYLE_SBML:
+    print("Parsing SBML Style model")
+    
+    # Get syncronizations from original model
+    syncs = []
+    for x in janiData["actions"]:
+      syncs.append(x["name"])
+
+    # Use syncs to find out modules reaction rates
+    # 1st: find the reaction rate automata
+
+    for automata in janiData["automata"]:
+      if "reaction_rates" == automata["name"]:
+        rr_mod = automata
+    
+    # 2nd: Correlate reaction rate to syncs
+    reactionRates = {}
+    for action in rr_mod["edges"]:
+      for sync in syncs:
+        if sync == action["action"]:
+          reactionRates[sync] = action["destinations"][0]["probability"]["comment"]
+
+    # 3rd: relate reaction rates to species
+    for automata in janiData["automata"]:
+      if "reaction_rates" != automata["name"]:
+        for edge in automata["edges"]:
+          for sync in syncs:
+            if sync == edge["action"]:
+              print(edge["destinations"][0]["assignments"][0]["ref"] + " correlated with "+ sync)
+
+  # else parse reaction rates of individual modules
+  else:
+    print("Parsing Traditional Style model")
+    for x in automatons:
+      if x != "reaction_rates":
+        myAbstract.modules.append(Module(x)) 
+
+  #### 
+    
+  #myAbstract.makeModel()
+
+  #print(type(janiData))
+  #print(type(janiData["automata"]))
+  #print(type(janiData["automata"][0]))
+  #print(janiData["automata"][0].keys())
+
+
